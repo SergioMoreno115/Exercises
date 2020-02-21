3fec2f2ed4ed8421aa837fd90ae7ab328368db16
SergioMoreno115 <smm3281@gmail.com>
2020-02-21 14:29:05 +0100

    maha

---
+++ area-circulo/area.js
@@ -0,0 +1,11 @@
+
+
+let radio;
+const pi = 3.1416;
+
+radio = prompt('Introduzca el radio:');
+
+const cuadrado = radio * 2;
+const area = pi * cuadrado;
+
+document.write(`El área del circulo es: ` + area);
