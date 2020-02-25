3ec215ee4b0138112434d8ea7755f0cb1adde745
SergioMoreno115 <smm3281@gmail.com>
2020-02-25 14:06:57 +0100

    si

---
+++ ejercicios-java/ejercicio1.js
@@ -0,0 +1,8 @@
+let primerNombre = "Carlos";
+let segundoNombre = "Alberto";
+let primerApellido = "Gómez";
+let segundoApellido = "Torres"
+let edad = "20";
+console.log("Hola, me llamo "+primerNombre+" "+segundoNombre+" "+primerApellido+" "+segundoApellido);
+console.log("Tengo "+edad+" años");
+console.log("Tengo "+edad+" años");
